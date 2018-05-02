2d20cd7f318e06f6f5d91d65c5d0960ec2bd6167
Lewis Moore <lewis.moore2707@gmail.com>
2018-05-02 09:52:22 +0100

    Add welcome message to landing page. Install request and add to tests

--- test/landingPageTest.js
+++ test/landingPageTest.js
@@ -1,15 +1,20 @@
 const chai = require('chai');
 const chaiHttp = require('chai-http');
-const app = require('../app');
+const app = require('../app.js');
 const expect = chai.expect;
+const request = require('request')
 chai.use(chaiHttp);
 
 describe('Landing Page', function(){
   it('should load successfully', function(){
-    chai.request(app)
-    .get('/')
-    .end(function(err, res){
+    request('http://localhost:8080', function(err, res){
       expect(res).to.have.status(200);
+    })
+  })
+
+  it('should render a welcome message', function(){
+    request('http://localhost:8080', function(err, res) {
+      expect(res.text).to.include('Welcome to CapitalOneder, your citizenship is a click away!')
     })
   })
 })
