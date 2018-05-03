0ed9fee8d22165822c67f84e6bf948a8bb57b251
Lewis Moore <lewis.moore2707@gmail.com>
2018-05-03 09:49:01 +0100

    Establish connection to Mongo database

--- app.js
+++ app.js
@@ -24,6 +24,21 @@ app.post('/success', function(req, res){
   res.render('successPage')
 })
 
+const MongoClient = require('mongodb').MongoClient;
+const url = 'mongodb://localhost:27017';
+const assert = require('assert');
+
+// Use connect method to connect to the server
+MongoClient.connect(url, function(err, client) {
+  assert.equal(null, err);
+  console.log("Connected successfully to server");
+
+  const db = client.db('test_db');
+  const collection = db.collection('documents');
+});
+
+
+
 app.listen(8080, () => console.log('Your on localhost 8080'))
 
 module.exports = app
