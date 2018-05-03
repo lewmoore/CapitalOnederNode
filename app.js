e403febe477e501de243cdb47063cbc8c1d34788
Lewis Moore <lewis.moore2707@gmail.com>
2018-05-03 10:04:55 +0100

    Add localhost to db connection string

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ app.post('/success', function(req, res){
 
 var MongoClient = require('mongodb').MongoClient
     , format = require('util').format;
-MongoClient.connect('mongodb://127.0.0.1:27017/CapitalOnederTest', function (err, db) {
+MongoClient.connect('mongodb://localhost:27017/CapitalOnederTest', function (err, db) {
     if (err) {
         throw err;
     } else {
