1c26105450207f142ab6b1fb86ff66f763b44e44
Lewis Moore <lewis.moore2707@gmail.com>
2018-06-19 16:50:23 +0100

    Dry up test with addition of user variable

--- test/successPageTest.js
+++ test/successPageTest.js
@@ -2,13 +2,21 @@ const chai = require('chai');
 const chaiHttp = require('chai-http');
 const app = require('../index');
 const expect = chai.expect;
-// const request = require('request')
 const Sinon = require('sinon')
 const spies = require('chai-spies');
 chai.use(spies);
 chai.use(chaiHttp);
 
 describe('Success Page', function(){
+  let user = {
+    "title": 'Mr',
+    'firstname': 'Lewis',
+    'surname': 'Moore',
+    'previouscountry': 'UK',
+    'sex': 'Male'
+  }
+
+
   it('can recieve post requests', function(){
     chai.request(app)
     .post('/citizen')
@@ -20,13 +28,7 @@ describe('Success Page', function(){
   it('should render the users form input', function(){
     chai.request(app)
     .post('/citizen')
-    .send({
-      "title": 'Mr',
-      'firstname': 'Lewis',
-      'surname': 'Moore',
-      'previouscountry': 'UK',
-      'sex': 'Male'
-    })
+    .send(user)
     .end(function(err, res){
       chai.expect(res.text).to.include('Mr')
       chai.expect(res.text).to.include('Lewis')
@@ -37,13 +39,7 @@ describe('Success Page', function(){
   it('should render entire welcome text with input from form', function(){
     chai.request(app)
     .post('/citizen')
-    .send({
-      "title": 'Mr',
-      'firstname': 'Lewis',
-      'surname': 'Moore',
-      'previouscountry': 'UK',
-      'sex': 'Male'
-    })
+    .send(user)
     .end(function(err, res){
       chai.expect(res.text).to.contain('Mr Lewis Moore, thank you for registering. Your Capital Oneder unique ID tag is:')
     })
